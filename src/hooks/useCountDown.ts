f9fa0f1b0829896696a6c8cc04defac6f3635240
HediMuhamad <hedymuhamad481@protonmail.com>
2024-08-06 00:52:31 +0300

    Removing Next template

---
+++ src/hooks/useCountDown.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export const useCountDown = (time: number) => {
+  const [count, setCount] = useState(time);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+  return count;
+};
